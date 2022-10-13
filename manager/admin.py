22995170b4e7381cdbcd462fbd8491397d9724d1
Andrei Kulakov <kulakov1@tut.by>
2022-10-13 17:27:49 +0300

    add models for course and group

--- manager/admin.py
+++ manager/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import CustomUser
+from .models import CustomUser, Course, Group
 
 admin.site.register(CustomUser)
+admin.site.register(Course)
+admin.site.register(Group)
--- manager/models.py
+++ manager/models.py
@@ -10,7 +10,28 @@ class CustomUser(AbstractUser):
 
     sex = models.CharField(verbose_name="Пол", max_length=4, choices=SEX_CHOICES)
     phone = models.CharField(verbose_name="Номер телефона", unique=True, blank=True, null=True, max_length=13)
-    birthday = models.DateField(verbose_name="День рождения", blank=True, null=True,)
+    birthday = models.DateField(verbose_name="День рождения", blank=True, null=True)
     interests = models.TextField(verbose_name="Интересы", blank=True, null=True, max_length=500)
     is_teacher = models.BooleanField(verbose_name="Преподаватель", default=False)
     is_student = models.BooleanField(verbose_name="Студент", default=False)
+
+
+class Course(models.Model):
+    name = models.CharField(verbose_name='Название курса', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    is_active = models.BooleanField(verbose_name='Активный', default=True)
+    start_date = models.DateField(verbose_name="День начала")
+    finish_date = models.DateField(verbose_name="День окончания")
+    teacher = models.ForeignKey('CustomUser',
+                                on_delete=models.CASCADE,
+                                limit_choices_to={'is_teacher': True},
+                                related_name='teachers')
+    group_size = models.PositiveIntegerField(verbose_name='Состав группы')
+    group_members = models.ManyToManyField('CustomUser',
+                                           related_name='group_members')
